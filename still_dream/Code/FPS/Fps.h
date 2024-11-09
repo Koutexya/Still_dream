c1e9944f945ab31d630c1fd0cbf398dd1bfa506b
406-08\student <fca>
2024-11-09 09:58:09 +0900

    Fpsクラスの追加

---
+++ still_dream/Code/FPS/Fps.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+namespace dream
+{
+    /// @brief  Fpsクラス
+    class Fps
+    {
+    public:
+        /// @brief  コンストラクタ
+        Fps();
+
+        /// @brief  デストラクタ
+        ~Fps();
+
+        /// @brief  更新処理
+        void Update();
+
+        /// @brief  デルタタイム取得
+        /// @return デルタタイム
+        float GetDeltaTime()const { return deltaTime; }
+
+    private:
+        float nowTime;  //現在の時間
+        float prevTime; //過去の時間
+        float deltaTime;    //デルタタイム
+    };
+}
