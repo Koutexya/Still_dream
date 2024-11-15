26b6f1c7b33701df822da07df4de5908c20c3fb9
407 <>
2024-11-15 17:35:09 +0900

    TitleのBackGroundクラスの追加

--- still_dream/Code/Object/Title/BackGround/BackGround.h
+++ still_dream/Code/Object/Title/BackGround/BackGround.h
@@ -1,8 +1,27 @@
 #pragma once
 #include<DxLib.h>
+#include"../../GameObject/GameObject.h"
 
 
 namespace dream
 {
+    class BackGround : public GameObject
+    {
+    public:
+        /// @brief  コンストラクタ
+        BackGround();
 
+        /// @brief  デストラクタ
+        ~BackGround();
+
+        /// @brief  更新処理
+        /// @param  デルタタイム
+        void Update(float deltaTime)override;
+
+        /// @brief  描画処理
+        void Draw()override;
+
+    private:
+        int BackGroundHandle;
+    };
 }
