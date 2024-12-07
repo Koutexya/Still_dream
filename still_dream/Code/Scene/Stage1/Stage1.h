a2fd54c1c4a1892db0ee29e2cb319073e9f8e2c5
406-08\student <fca>
2024-12-07 11:17:40 +0900

    SceneStage1の追加

---
+++ still_dream/Code/Scene/Stage1/Stage1.h
@@ -0,0 +1,29 @@
+#pragma once
+#include"../SceneBase/SceneBase.h"
+#include"../../Object/GameObjectManager/GameObjectManager.h"
+#include"../../Object/GameObject/Stage1ObjectTag.h"
+#include"../../Object/Stage1/BackGround/BackGround.h"
+
+namespace dream
+{
+    /// @brief  Stage1画面の進行管理クラス
+    class Stage1 :public SceneBase
+    {
+    public:
+        /// @brief  コンストラクタ
+        Stage1();
+
+        /// @brief  デストラクタ
+        ~Stage1();
+
+        /// @brief  シーン更新処理
+        /// @param deltaTime=デルタタイム
+        /// @return 次のフレームシーン
+        SceneBase* Update(const float deltaTime)override;
+
+        /// @brief  シーン更新処理
+        void Draw()override;
+
+    private:
+    };
+}
