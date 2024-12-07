91724076c16696ae23ad4f9e2a30c0236e2812df
406-08\student <fca>
2024-12-07 14:14:43 +0900

    Stage1Uiの追加

---
+++ still_dream/Code/Object/Stage1/Ui/Stage1Ui.cpp
@@ -0,0 +1,26 @@
+#include"Stage1Ui.h"
+
+namespace dream
+{
+    Stage1Ui::Stage1Ui()
+        :GameObject(stage1ObjectTag.UI)
+    {
+
+    }
+
+    Stage1Ui::~Stage1Ui()
+    {
+
+    }
+
+    void Stage1Ui::Update(float deltaTime)
+    {
+        Draw();
+    }
+
+    void Stage1Ui::Draw()
+    {
+        //âÊñ ÉTÉCÉY(1920,1080)
+        DrawLine(0, 540, 1920, 540, GetColor(255, 255, 0));
+    }
+}
